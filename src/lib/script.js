88caaeaa42b2b01c58341a05d76a2309c9119f90
zhyrgalbek <zhyrgalbek.kamalov@gmail.com>
2019-08-01 10:32:09 +0300

    add script.js

---
+++ src/lib/script.js
@@ -0,0 +1,58 @@
+$(function(){
+	var w = $(window).width();
+	if(w > 480){
+		$(".main-otzyv .container").removeClass("padding");
+	}
+	// if(w > 768){
+	// 	$(".nav ul li").hover(function(){
+	// 		$(this).find("a").addClass("animated tada");
+	// 	}, function(){
+	// 		$(this).find("a").removeClass("animated tada");
+	// 	})
+	// }
+	var $hamburger = $(".hamburger");
+	var $nav = $(".nav");
+	$hamburger.on("click", function(){
+		$(this).toggleClass("is-active");
+		$nav.toggleClass("left");
+	});
+	$('.btn a').click(function(){
+		var target = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top-102
+		}, 500);
+	});
+	$('.nav ul li a[href^="#"]').click(function(){
+		var target = $(this).attr("href");
+		$("html, body").animate({
+			scrollTop: $(target).offset().top-102
+		}, 1000);
+	});
+	$(window).scroll(function(){
+		var top = $(document).scrollTop();
+		if(top > 500){
+			$(".header-head").addClass("fixed");
+		} else {
+			$(".header-head").removeClass("fixed");
+		}
+	});
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		nav: false,
+		navText: ["", ""],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1,
+				nav: true
+			},
+			1000: {
+				items: 1,
+				nav: true
+			}
+		}
+	})
+});
